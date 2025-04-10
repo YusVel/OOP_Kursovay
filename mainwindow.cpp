d97c1f90c905338c45d7157be575074252d1e030
Yusup Velimetov <yusupvelimetov@mail.ru>
2025-04-11 01:10:46 +0300

    555

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,20 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{}
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent)
+{
+    icon.addFile("mainWindowIco.ico");
+    this->setWindowIcon(icon);
+    this->setAttribute(Qt::WA_DeleteOnClose,true);
+    this->setWindowTitle("Курсовая работа по ООП (class CAR)");
+    this->resize(600,700);
+    this->setFixedWidth(600);
+
+}
 
 MainWindow::~MainWindow() {}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    qDebug()<<"close()";
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,15 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QIcon>
 #include "CAR.h"
 #include "VECTOR.h"
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    QIcon icon;
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+private:
+    void closeEvent(QCloseEvent *event) override;
 };
 #endif // MAINWINDOW_H
