e0f3e23d490ca5c0caf8ffddfec329d956c0ca4a
Yusup Velimetov <yusupvelimetov@mail.ru>
2025-04-13 14:52:37 +0300

    toolbox added

---
+++ build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Kursovay_FILE E:/Qtprojects/Kursovay/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/Kursovay.exe)
+set(__QT_DEPLOY_TARGET_Kursovay_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Kursovay_RUNTIME_DLLS E:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;E:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;E:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
